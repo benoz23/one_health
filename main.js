70b009e4536d40713795c759f170d7c2567ea4a6
benoz23 <gianluca.benotto@gmail.com>
2023-11-24 13:44:33 +0100

    logs

--- main.js
+++ main.js
@@ -3,18 +3,24 @@ AFRAME.registerComponent('markerhandler', {
     const markerId = this.el.id;
 
     this.el.sceneEl.addEventListener('markerFound', () => {
+      console.log(`Marker ${markerId} found`);
+      
       // Disable other markers
       this.el.sceneEl.querySelectorAll('[markerhandler]').forEach((marker) => {
-        if (marker.id !== markerId) {
+        if (marker !== this.el) {
           marker.setAttribute('visible', false);
+          console.log(`Hiding marker ${marker.id}`);
         }
       });
     });
 
     this.el.sceneEl.addEventListener('markerLost', () => {
+      console.log(`Marker ${markerId} lost`);
+
       // Enable all markers
       this.el.sceneEl.querySelectorAll('[markerhandler]').forEach((marker) => {
         marker.setAttribute('visible', true);
+        console.log(`Showing marker ${marker.id}`);
       });
     });
   }
